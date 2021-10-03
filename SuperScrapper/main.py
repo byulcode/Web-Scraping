3938e4663a3978369eebe20b6e63e512c6b20301
bybit0408 <bybit0408@gmail.com>
2021-10-03 23:30:35 +0900

    make fake db

--- SuperScrapper/main.py
+++ SuperScrapper/main.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
+db = {}
+
+
 @app.route("/")
 def home():
-  return render_template("potato.html")
+    return render_template("potato.html")
 
-@app.route("/<username>")
-def contact(username): #함수 이름 달라도 됨
-  return f"Hello {username} how are you doing"
 
-app.run(host="0.0.0.0") #repl.it을 위해 있는것. 로컬에선 지워
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)  # db에 있는지 찾기
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs  # 결과 db에 저장
+    else:
+        return redirect("/")  # home으로 되돌아감
+    return render_template("report.html",
+                           searchingBy=word,
+                           resultsNumber=len(jobs),
+                           jobs=jobs
+                           )
+
+
+app.run(host="0.0.0.0")  # repl.it을 위해 있는것. 로컬에선 지워
